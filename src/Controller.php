3361829b2f3cbc4d3c0d53697444c4dc402c1482
Bagus A Priambada <iam.adty@gmail.com>
2020-11-30 10:21:21 +0700

    try json params

--- src/Controller.php
+++ src/Controller.php
@@ -76,6 +76,13 @@ class Controller extends \CodeIgniter\RESTful\ResourceController
     public function index()
     {
         $crud = $this->_doCRUD(function ($query = null, ?\CI4Xpander\Model $model, ?string $entity, ?string $table) {
+            $jsonParams = $this->request->getJSON();
+            if (!is_null($jsonParams)) {
+                if (isset($jsonParams->query)) {
+                    $query = \CI4Xpander\Helpers\Database\Query\Builder::fromJSON($query, $jsonParams->query);
+                }
+            }
+
             $query = \CI4Xpander\Helpers\Database\Query\Builder::forceQueryToString($query, \Config\Database::connect(), $model);
 
             if (is_null($query)) {
@@ -356,5 +363,240 @@ class Controller extends \CodeIgniter\RESTful\ResourceController
         }
 
         return null;
+    }
+
+    public function data()
+    {
+        $table = null;
+
+        /** @var \CI4Xpander\Model */
+        $model = $this->CRUD['model'] ?? null;
+
+        if (!is_null($model)) {
+            if (!is_a($model, \CI4Xpander\Model::class)) {
+                $model = $model::create();
+            }
+            $table = $model->getTable();
+        }
+
+        $columns = $this->CRUD['index']['columns'] ?? [];
+
+        $query = null;
+        if (isset($this->CRUD['index']['query'])) {
+            $query = $this->CRUD['index']['query'];
+
+            if (is_callable($query)) {
+                $query = $query(\Config\Database::connect(), $model);
+            }
+        } else {
+            if (!is_null($model)) {
+                $query = $model->builder();
+            }
+        }
+
+        $draw = $this->request->getGet('draw');
+        $columnsGet = $this->request->getGet('columns');
+        $order = $this->request->getGet('order');
+        $start = $this->request->getGet('start');
+        $length = $this->request->getGet('length');
+        $search = preg_replace('/\s+/', '%', $this->request->getGet('search')) ?? '';
+
+        /** @var \CodeIgniter\Database\BaseBuilder */
+        $data = \Config\Database::connect()->table('ci4x_dashboard_data_temporary_table');
+
+        /** @var \CodeIgniter\Database\BaseBuilder */
+        $recordsFiltered = \Config\Database::connect()->table('ci4x_dashboard_data_temporary_table');
+
+        if (!is_string($query)) {
+            $compiledQuery = $query->getCompiledSelect();
+        } else {
+            $compiledQuery = $query;
+        }
+
+        $data->from("({$compiledQuery}) ci4x_dashboard_data_temporary_table", true);
+        $recordsFiltered->from("({$compiledQuery}) ci4x_dashboard_data_temporary_table", true);
+
+        $recordsTotal = \Config\Database::connect()->table('ci4x_dashboard_data_temporary_table')->from("({$compiledQuery}) ci4x_dashboard_data_temporary_table", true);
+
+        if (isset($search)) {
+            if (isset($search['value'])) {
+                if (!empty($search['value'])) {
+                    if (isset($columnsGet)) {
+                        if (is_array($columnsGet)) {
+                            $data->groupStart();
+                            $recordsFiltered->groupStart();
+                            $i = 0;
+                            foreach ($columnsGet as $column) {
+                                if ($column['searchable'] == 'true') {
+                                    $c = $columns[$column['data']];
+
+                                    $searchValue = trim(preg_replace('/\s+/', '%', $search['value']));
+
+                                    if (is_array($c)) {
+                                        if (isset($c['value'])) {
+                                            if (is_array($c['value'])) {
+                                                if ($i == 0) {
+                                                    $data->groupStart();
+                                                    $recordsFiltered->groupStart();
+                                                } else {
+                                                    $data->orGroupStart();
+                                                    $recordsFiltered->orGroupStart();
+                                                }
+                                                $j = 0;
+                                                foreach ($c['value'] as $cKey => $cValue) {
+                                                    if (is_numeric($cKey)) {
+                                                        $fToS = $cValue;
+                                                    } else {
+                                                        $fToS = $cKey;
+                                                    }
+
+                                                    if ($j == 0) {
+                                                        $data->like("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                        $recordsFiltered->like("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                    } else {
+                                                        $data->orLike("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                        $recordsFiltered->orLike("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                    }
+
+                                                    $j++;
+                                                }
+                                                $data->groupEnd();
+                                                $recordsFiltered->groupEnd();
+                                            } else {
+                                                if ($i == 0) {
+                                                    $data->like("{$c['value']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                    $recordsFiltered->like("{$c['value']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                } else {
+                                                    $data->orLike("{$c['value']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                    $recordsFiltered->orLike("{$c['value']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                }
+                                            }
+                                        } else {
+                                            if ($i == 0) {
+                                                $data->like("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                $recordsFiltered->like("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                            } else {
+                                                $data->orLike("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                $recordsFiltered->orLike("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                            }
+                                        }
+                                    } else {
+                                        if ($i == 0) {
+                                            $data->like("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                            $recordsFiltered->like("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                        } else {
+                                            $data->orLike("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                            $recordsFiltered->orLike("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                        }
+                                    }
+
+                                    $i++;
+                                }
+                            }
+
+                            $data->groupEnd();
+                            $recordsFiltered->groupEnd();
+                        }
+                    }
+                }
+            }
+        }
+
+        if (isset($columnsGet)) {
+            if (is_array($columnsGet)) {
+                foreach ($columnsGet as $column) {
+                    if ($column['searchable'] == 'true') {
+                        if (!empty($column['search']['value'])) {
+                            $c = $columns[$column['data']];
+
+                            $searchValue = trim(preg_replace('/\s+/', '%', $column['search']['value']));
+
+                            if (is_array($c)) {
+                                if (isset($c['value'])) {
+                                    if (is_array($c['value'])) {
+                                        $data->groupStart();
+                                        $recordsFiltered->groupStart();
+                                        $i = 0;
+                                        foreach ($c['value'] as $cKey => $cValue) {
+                                            if (is_numeric($cKey)) {
+                                                $fToS = $cValue;
+                                            } else {
+                                                $fToS = $cKey;
+                                            }
+
+                                            if ($i == 0) {
+                                                $data->like("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                $recordsFiltered->like("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                            } else {
+                                                $data->orLike("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                                $recordsFiltered->orLike("{$fToS}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                            }
+
+                                            $i++;
+                                        }
+                                        $data->groupEnd();
+                                        $recordsFiltered->groupEnd();
+                                    } elseif (is_callable($c['value'])) {
+                                        $data->like("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                        $recordsFiltered->like("{$column['data']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                    } else {
+                                        $data->like("{$c['value']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                        $recordsFiltered->like("{$c['value']}::TEXT", \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                    }
+                                } else {
+                                    $data->like($column['data'] . '::TEXT', \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                    $recordsFiltered->like($column['data'] . '::TEXT', \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                }
+                            } else {
+                                $data->like($column['data'] . '::TEXT', \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                                $recordsFiltered->like($column['data'] . '::TEXT', \Config\Database::connect()->escape("%{$searchValue}%"), 'none', false, true);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (isset($order)) {
+            if (is_array($order)) {
+                foreach ($order as $columnOrder) {
+                    if (isset($columnsGet)) {
+                        if ($columnsGet[intval($columnOrder['column'])]['orderable'] == 'true') {
+                            $colKey = $columnsGet[intval($columnOrder['column'])]['data'];
+                            if (is_array($columns[$colKey])) {
+                                if (isset($columns[$colKey]['value'])) {
+                                    if (is_array($columns[$colKey]['value'])) {
+                                        foreach ($columns[$colKey]['value'] as $field => $name) {
+                                            $data->orderBy(is_string($field) ? $field : $name, $columnOrder['dir']);
+                                        }
+                                    } elseif (is_string($columns[$colKey]['value'])) {
+                                        $data->orderBy($columns[$colKey]['value'], $columnOrder['dir']);
+                                    }
+                                } else {
+                                    $data->orderBy($colKey, $columnOrder['dir']);
+                                }
+                            } else {
+                                $data->orderBy($columnsGet[intval($columnOrder['column'])]['data'], $columnOrder['dir']);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        if (isset($start)) {
+            $data->offset(intval($start));
+        }
+
+        if (isset($length)) {
+            $data->limit(intval($length));
+        }
+
+        return $this->response->setJSON([
+            'draw' => isset($draw) ? intval($draw) : 0,
+            'recordsTotal' => $recordsTotal->countAllResults(),
+            'recordsFiltered' => $recordsFiltered->countAllResults(),
+            'data' => $data->get()->getResult()
+        ]);
     }
 }
